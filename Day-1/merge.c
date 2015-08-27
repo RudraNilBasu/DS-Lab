981fd8efeedbd4e87a6c16656e955852363289ff
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-08-27 17:18:52 +0530

    Create merge.c

---
+++ Day-1/merge.c
@@ -0,0 +1,82 @@
+// merging two arrays in sorted order without using any sorting technique
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Thou art RNBfied\n");
+	int a[10],b[5],c[15];
+	int lol=0,pos=0,posa=0,posb=0,yolo=0;
+	printf("Insert 10 elements in 1st array in sorted order\n");
+	int i;
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("Insert 5 elements in 2nd array in sorted order\n");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&b[i]);
+	}//printf("lol\n");
+	// merging
+	while(pos!=15)
+	{
+		/*if(lol==1)
+		{
+			printf("lol\n");
+			for(i=posb;i<5;i++)
+			{
+				c[pos++]=b[posb++];
+			}
+		}
+		else if(yolo=1)
+		{
+			for(i=posa;i<10;i++)
+			{
+				c[pos++]=a[posa++];
+			}
+		}*/
+		//else
+		//{
+			if(lol=1)
+			{
+				//printf("HAHA \n");
+			}
+			if(a[posa]<b[posb])
+			{
+					//posa++;
+					//printf("lol1 position is %d\n",posa);
+					c[pos++]=a[posa++];
+					if(posa>9)
+					{
+						for(i=posb;i<5;i++)
+						{
+							c[pos++]=b[i];
+						}
+						break;
+					}
+					//break;
+			}
+			else
+			{
+				//printf("lol2\n");
+				c[pos++]=b[posb++];
+				if(posb>5)
+				{
+					//posb=4;
+					yolo=1;
+					for(i=posa;i<10;i++)
+					{
+						c[pos++]=a[i];
+					}
+					break;
+				}
+			}
+		//}
+	}
+	//printf("YOLO\n");
+	for(i=0;i<15;i++)
+	{
+		printf("%d\n",c[i]);
+	}
+}
