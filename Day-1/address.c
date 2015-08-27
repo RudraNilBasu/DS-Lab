01dc9114f8d0abc8ffdf2fb87e698f84a8ac0255
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-08-27 17:36:46 +0530

    Create address.c

---
+++ Day-1/address.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3];
+	printf("Enter the array");
+	//int i,j;
+	/*
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	*/
+	//printf("%d",a);
+	int ba,c,x,i,j;
+	printf("1.Row Major.\n2.Column Major");
+	scanf("%d",&c);
+	printf("Enter BA\n");
+	scanf("%d",&ba);
+	printf("Enter i and j\n");
+	scanf("%d %d",&i,&j);
+	if(c==1)
+	{
+		x=ba+((i*3)+j)*sizeof(int);
+	}
+	else
+	{
+		x=ba+((j*3)+i)*sizeof(int);
+	}
+	printf("ans = %d",x);
+}
