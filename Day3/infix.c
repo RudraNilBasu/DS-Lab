be04c40fe8690c4340fa068efb22a45a5c330fe8
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-09-30 23:43:25 +0530

    Completed checking correctness of infix notation

--- Day3/infix.c
+++ Day3/infix.c
@@ -1,15 +1,64 @@
 //To check whether a infix expression is correct or not
-// Done upto checking the number of Brackets, not operands, so it is a WIP :P 
-//TODO : Check if the popped element is the openning of the closing bracket type, if no, then WA [DONE]
+// Done upto checking the number of Brackets,  operands.
+// May be rough, Please report in case of errors
+// Done by - Rudra Nil Basu, @PriZm
 #include<stdio.h>
 int top=-1;
 char inf[50];
 char s[50];
+char inf2[50];
+
+int isoperand(int i)
+{
+    if(inf2[i]>='a' && inf2[i]<='z')
+        return 1;
+    return 0;
+}
+
+int isoperator(int i)
+{
+    if(inf2[i]=='+')
+    {
+        return 1;
+    }
+    if(inf2[i]=='-')
+    {
+        return 1;
+    }
+    if(inf2[i]=='*')
+    {
+        return 1;
+    }
+    if(inf2[i]=='/')
+    {
+        return 1;
+    }
+    if(inf2[i]=='^')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int rem()
+{
+    int i,k=0;
+    for(i=0;inf[i]!=';';i++)
+    {
+        if(inf[i]=='(' ||inf[i]==')' ||inf[i]=='{' ||inf[i]=='}' ||inf[i]=='[' ||inf[i]==']' )
+        //if(inf[i]!='(' ||inf[i]!=')' ||inf[i]!='{' ||inf[i]!='}' ||inf[i]!='[' ||inf[i]!=']' )
+            {
+                continue;
+            }
+            inf2[k++]=inf[i];
+    }
+    inf2[k]=';';
+}
 
 int main()
 {
     scanf("%s",inf);
-    int i,flag=0;
+    int i,flag=0,part1=0,part2=1;
     for(i=0;i<strlen(inf);i++)
     {
         if(inf[i]=='(')
@@ -54,18 +103,52 @@ int main()
     }
     if(flag==1)
     {
-        printf("Incorrect Bracket placements\n");
+        part1=0;//printf("Incorrect Bracket placements\n");
     }
     else
     {
         if(top==-1)
         {
-            printf("Correct.\n");
+            part1=1;
         }
         else
         {
-            printf("Not Correct.\n");
+            part1=0;
         }
+    }
+    inf[strlen(inf)]=';';
+    rem();//to remove all brackets
+    for(i=0;inf2[i]!=';';i++)
+    {
+        if(isoperand(i) && isoperand(i+1))
+        {
+            part2=0;
+            break;
+        }
+        if(inf2[i+1]!=';')
+        {
+            if( isoperand(i) && isoperator(i+1))
+            {
+                if(inf2[i+2]==';')
+                {
+                    part2=0;
+                    break;
+                }
+                else if( !isoperand(i+2) )
+                {
+                    part2=0;
+                    break;
+                }
+            }
+        }
+    }
+    if( part1 && part2)
+    {
+        printf("Correct\n");
+    }
+    else
+    {
+        printf("Incorrect:/\n");
     }
     return 0;
 }
