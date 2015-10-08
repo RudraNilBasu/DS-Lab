0ff92e03d536859814df6c3ce44975a11e5e193f
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-10-08 16:41:59 +0530

    Create stack.c

---
+++ Day4/stack.c
@@ -0,0 +1,258 @@
+#include<stdio.h>
+#define intd(x) scanf("%d",&x)
+
+struct node 
+{
+	int data;
+	struct node *next;
+};
+
+struct node *list;
+
+int insert_beg()
+{
+	int n;
+	struct node *p;
+	p=(struct node *)malloc(sizeof(struct node));
+	printf("Enter the data \n");
+	intd(n);
+	p->data=n;
+	p->next=list;
+	list=p;
+}
+
+int insert_end()
+{
+	int n;
+	struct node *p,*q;
+	p=(struct node *)malloc(sizeof(struct node));
+	q=(struct node *)malloc(sizeof(struct node));
+	if(list==NULL)
+	{
+		printf("Enter the 1st Data\n");
+		intd(n);
+		p->data=n;
+		p->next=NULL;
+		list=p;
+		//printf("LOL %d %d\n",list->next, p->next);
+		/*
+		if(list==NULL)
+		{
+			printf("haha\n");
+		}
+		*/
+	}
+	else
+	{
+		for(q=list;q->next!=NULL;q=q->next);
+		printf("Enter the data\n");
+		intd(n);
+		p->data=n;
+		p->next=NULL;
+		q->next=p;
+	}
+	return 0;
+}
+
+int insert_pos()
+{
+	int pos,n,c=0,k=0;
+	printf("Enter the position after which the node will be inserted\n");
+	intd(pos);
+	printf("Enter data\n");
+	intd(n);
+	struct node *p;
+	struct node *q;
+	q=(struct node *)malloc(sizeof(struct node));
+	for(p=list;p!=NULL;p=p->next)
+	{
+		c++;
+	}
+	if(pos>c)
+	{
+		printf("Out of bounds\n");
+	}
+	else
+	{
+		c=0;
+		for(p=list;p!=NULL;p=p->next)
+		{
+			c++;
+			if(c==pos)
+			{
+				q->data=n;
+				q->next=p->next;
+				p->next=q;
+				// previous next = p;
+				break;
+			}
+		}
+	}
+}
+
+int delEnd()
+{
+	struct node *p,*q;
+	p=(struct node *)malloc(sizeof(struct node));
+	q=(struct node *)malloc(sizeof(struct node));
+	//for(p=list;p->next!=NULL;p=p->next);
+	//p->next=NULL;
+	//printf("DEL  : %d\n",p->data);
+	p=list;
+	if(list==NULL)
+	{
+		printf("Empty\n");
+	}
+	else
+	{
+		
+		while(p->next!=NULL)
+		{
+			q=p;
+			p=p->next;
+		}
+		q->next=NULL;
+	}
+	//q->next=NULL;
+}
+
+int delBeg()
+{
+	if(list==NULL)
+	{
+		printf("Empty\n");
+	}
+	else
+	{
+		//list->next=(list->next)->next;
+		list=list->next;
+	}
+}
+
+int delPos()
+{
+	int pos,n,c=0;
+	printf("Enter the position\n");
+	intd(pos);
+	if(pos==1)
+	{
+		delBeg();
+	}
+	struct node *p;
+	for(p=list;p!=NULL;p=p->next)
+	{
+		c++;
+	}
+	if(c==pos)
+	{
+		delEnd();
+	}
+	if(pos>c)
+	{
+		printf("Not Possible\n");
+	}
+	else
+	{
+		c=0;
+		for(p=list;p!=NULL;p=p->next)
+		{
+			c++;
+			if(c==pos)
+			{
+				p->next=(p->next)->next;
+				break;
+			}
+		}
+	}
+	return 0;
+}
+
+int display()
+{
+	struct node *p;
+	p=(struct node *)malloc(sizeof(struct node));
+	if(list==NULL)
+	{
+		printf("Empty\n");
+	}
+	else
+	{
+		printf("The list is : \n");
+		for(p=list;p!=NULL;p=p->next)
+		{
+			printf("%d\n",p->data);
+		}
+	}
+}
+
+int main()
+{
+	struct node *p;
+	p=(struct node *)malloc(sizeof(struct node));
+	//list=p;
+	/*
+	printf("Enter no of elements in the linked list\n");
+	int t,n;
+	intd(t);
+	while(t--)
+	{
+		insert_end();
+	}
+	display();
+	printf("YOLO\n");
+	delBeg();
+	display();
+	insert_beg();
+	display();
+	insert_pos();
+	display();
+	delEnd();
+	display();
+	delPos();
+	display();
+	//printf("LOL\n");
+	//delBeg();
+	//display();
+	*/
+	int choice=0;
+	while(1)
+	{
+		printf("1.Push\n2.Pop\n3.Display\n4.Exit\n");
+		intd(choice);
+		if(choice==1)
+		{
+			insert_beg();
+		}
+		else if(choice==2)
+		{
+			delBeg();
+		}
+		else if(choice==3)
+		{
+			display();
+		}
+		else if(choice==4)
+		{
+			break;
+		}
+		/*
+		else if(choice==5)
+		{
+			delPos();
+		}
+		else if(choice==6)
+		{
+			insert_pos();
+		}
+		else if(choice==7)
+		{
+			break;
+		}
+		else if(choice==8)
+		{
+			display();
+		}
+		*/
+	}
+	return 0;
+}
