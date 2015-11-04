007d24001c7a040f0244fe303b1c57b6328b8bf0
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2015-11-04 23:34:05 +0530

    Added cq.c and dequeue.c

---
+++ Day4/cq.c
@@ -0,0 +1,130 @@
+/*
+Implementation of Circular Queue using array
+Done by : Rudra Nil Basu
+*/
+
+#include<stdio.h>
+#define intd(x) scanf("%d",&x)
+
+#define SIZE 5
+
+int front=0,rear=-1;
+int a[SIZE];
+
+
+int del()
+{
+    if(front==0 && rear == -1)
+    {
+        printf("Underflow\n");
+    }
+    else if(front==rear)
+    {
+        printf("Element deleted : %d\n",a[front]);
+        front=0;
+        rear=-1;
+    }
+    else if(front==SIZE-1) // if the element to be deleted is the last element of the array
+    {
+        printf("Element deleted : %d\n",a[front]);
+        front=0;
+    }
+    else
+    {
+        printf("Element deleted : %d\n",a[front++]);
+    }
+    return 0;
+}
+
+
+int insrt(int x)
+{
+    // insert a number to the queue
+    if(front==0 && rear==SIZE-1)
+    {
+        printf("Queue Overflow.\n");
+    }
+    else if(front>0 && rear == front-1)
+    {
+        printf("Queue Overflow.\n");
+    }
+    else
+    {
+        if(front>0 && rear==SIZE-1) // when the array end is full but there is some element empty from the front
+        {
+            rear=0;
+            a[rear]=x;
+        }
+        else
+        {
+            if(rear!=front-1)
+            {
+                a[++rear]=x;
+            }
+            else if(front==0 && rear==-1)
+            {
+                a[++rear]=x; // when the queue is empty
+            }
+        }
+    }
+    return 0;
+}
+
+int display()
+{
+    int i;
+    if(front==0 && rear==-1)
+    {
+        printf("Queue Empty.\n");
+    }
+    else if(front>rear)
+    {
+        printf("Queue is : .\n");
+        for(i=front;i<SIZE;i++)
+        {
+            printf("%d\n",a[i]);
+        }
+        for(i=0;i<=rear;i++)
+        {
+            printf("%d\n",a[i]);
+        }
+    }
+    else
+    {
+        printf("Queue is : .\n");
+        for(i=front;i<=rear;i++)
+        {
+            printf("%d\n",a[i]);
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int choice,x;
+    while(1)
+    {
+        printf("1.Insert\n2.Delete.\n3.Display.\n4.EXIT\n");
+        intd(choice);
+        if(choice==1)
+        {
+            printf("Enter no to insert\n");
+            intd(x);
+            insrt(x);
+        }
+        if(choice==2)
+        {
+            del();
+        }
+        if(choice==3)
+        {
+            display();
+        }
+        if(choice==4)
+        {
+            break;
+        }
+    }
+    return 0;
+}
---
+++ Day4/dequeue.c
@@ -0,0 +1,228 @@
+/*
+
+*/
+
+#include<stdio.h>
+#define intd(x) scanf("%d",&x)
+
+struct node // basic structure of all nodes
+{
+	int data;
+	struct node *next;
+};
+
+struct node *list; // pointer to the first list
+
+int insert_beg() //insert in the beginning of the list
+{
+	int n;
+	struct node *p;
+	p=(struct node *)malloc(sizeof(struct node));
+	printf("Enter the data \n");
+	intd(n);
+	p->data=n;
+	p->next=list;
+	list=p;
+}
+
+int insert_end()
+{
+	int n;
+	struct node *p,*q;
+	p=(struct node *)malloc(sizeof(struct node));
+	q=(struct node *)malloc(sizeof(struct node));
+	if(list==NULL)
+	{
+		printf("Enter the 1st Data\n");
+		intd(n);
+		p->data=n;
+		p->next=NULL;
+		list=p;
+
+	}
+	else
+	{
+		for(q=list;q->next!=NULL;q=q->next);
+		printf("Enter the data\n");
+		intd(n);
+		p->data=n;
+		p->next=NULL;
+		q->next=p;
+	}
+	return 0;
+}
+
+int insert_pos()
+{
+	int pos,n,c=0,k=0;
+	printf("Enter the position after which the node will be inserted\n");
+	intd(pos);
+	printf("Enter data\n");
+	intd(n);
+	struct node *p;
+	struct node *q;
+	q=(struct node *)malloc(sizeof(struct node));
+	for(p=list;p!=NULL;p=p->next)
+	{
+		c++;
+	}
+	if(pos>c)
+	{
+		printf("Out of bounds\n");
+	}
+	else
+	{
+		c=0;
+		for(p=list;p!=NULL;p=p->next)
+		{
+			c++;
+			if(c==pos)
+			{
+				q->data=n;
+				q->next=p->next;
+				p->next=q;
+				break;
+			}
+		}
+	}
+}
+
+int delEnd()
+{
+	struct node *p,*q;
+	p=(struct node *)malloc(sizeof(struct node));
+	q=(struct node *)malloc(sizeof(struct node));
+	if(list==NULL)  // checking whether the list is empty
+	{
+		printf("Empty\n");
+	}
+	else
+	{
+		p=list;
+		if(p->next == NULL)
+        {
+            // if there is only one node in the list
+            p=NULL;
+            list=NULL;
+        }
+		while(p->next!=NULL) // moving till the 2nd last node
+		{
+			q=p;
+			p=p->next;
+		}
+		q->next=NULL;
+	}
+	//q->next=NULL;
+}
+
+int delBeg()
+{
+	if(list==NULL)
+	{
+		printf("Empty\n");
+	}
+	else
+	{
+		//list->next=(list->next)->next;
+		list=list->next;
+	}
+}
+
+int delPos()
+{
+	int pos,n,c=0;
+	printf("Enter the position\n");
+	intd(pos);
+	if(pos==1)
+	{
+		delBeg();
+		return 0;
+	}
+	struct node *p;
+	for(p=list;p!=NULL;p=p->next)
+	{
+		c++;
+	}
+	if(c==pos)
+	{
+		delEnd();
+	}
+	if(pos>c)
+	{
+		printf("Not Possible\n");
+	}
+	else
+	{
+		c=0;
+		for(p=list;p!=NULL;p=p->next)
+		{
+			c++;
+			if(c==pos-1)
+			{
+				p->next=(p->next)->next;
+				break;
+			}
+		}
+	}
+	return 0;
+}
+
+int display()
+{
+	struct node *p;
+	p=(struct node *)malloc(sizeof(struct node));
+	if(list==NULL)
+	{
+		printf("Empty\n");
+	}
+	else
+	{
+		printf("The list is : \n");
+		for(p=list;p!=NULL;p=p->next)
+		{
+			printf("%d\n",p->data);
+		}
+	}
+	return 0;
+}
+
+
+int main()
+{
+    int n;
+    while(1)
+    {
+        printf("1.insert at front\n");
+        printf("2.insert at rear\n");
+        printf("3.Delete from front\n");
+        printf("4.Delete from rear\n");
+        printf("5.Display\n");
+        printf("6.Exit\n");
+        intd(n);
+        if(n==1)
+        {
+            insert_beg();
+        }
+        else if(n==2)
+        {
+            insert_end();
+        }
+        else if(n==3)
+        {
+            delBeg();
+        }
+        else if(n==4)
+        {
+            delEnd();
+        }
+        else if(n==5)
+        {
+            display();
+        }
+        else if(n==6)
+        {
+            break;
+        }
+    }
+    return 0;
+}
