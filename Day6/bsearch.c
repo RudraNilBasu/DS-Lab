72ad2004e7b254b1570585b1e70989efb9842b9f
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-11-20 21:44:00 +0530

    Binary Search

---
+++ Day6/bsearch.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+	int a[5];
+	int n,i; // no to search
+	printf("Enter the sorted array\n");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("Enter the number to search\n");
+	scanf("%d",&n);
+	int low=0,high=4,mid=(low+high)/2,flag=0;
+	while(low<=high)
+	{
+		mid=(low+high)/2;
+		if(a[mid]==n)
+		{
+			printf("Found\n");
+			flag=1;
+			break;
+		}
+		else
+		{
+			if(n>a[mid])
+			{
+				low=mid+1;
+			}
+			else
+			{
+				high=mid-1;
+			}
+		}
+	}
+	if(!flag)
+	{
+		printf("Not Found\n");
+	}
+	return 0;
+}
