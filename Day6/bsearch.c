8922fde1c9a838b58d6e4d81eb6bceeb9d01531e
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-11-23 00:42:02 +0530

    Update bsearch.c

--- Day6/bsearch.c
+++ Day6/bsearch.c
@@ -3,7 +3,7 @@ int main()
 {
 	int a[5];
 	int n,i; // no to search
-	printf("Enter the sorted array\n");
+	printf("Enter the sorted array\n"); // considering sorted array
 	for(i=0;i<5;i++)
 	{
 		scanf("%d",&a[i]);
