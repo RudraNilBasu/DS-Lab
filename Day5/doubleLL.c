8a92b22573b841a6e72b66ba265e748db2beb0b1
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-04-04 22:54:32 +0530

    Update doubleLL.c

--- Day5/doubleLL.c
+++ Day5/doubleLL.c
@@ -50,7 +50,7 @@ int display()
 	}
 	else
 	{
-		printf("\n\nThe list is \n");
+		printf("\n\nThe list is \n"); // printing if the list is not empty
 		for(p=list;p!=NULL;p=p->next)
 		{
 			printf("%d\n",p->data);  // printing the data part of the nodes of the list
